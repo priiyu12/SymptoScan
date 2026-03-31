ae9f5dec74842f8cb838672bcd0f0bf5930150a6
Priya Shah <priyashah@Priyas-MacBook-Air.local>
2026-03-31 20:41:34 +0530

    Added my folder

---
+++ backend/prediction/train_model.py
@@ -0,0 +1,15 @@
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+import joblib
+
+data = pd.read_csv("../dataset/disease_dataset.csv")
+
+X = data.drop("disease", axis=1)
+y = data["disease"]
+
+model = RandomForestClassifier()
+model.fit(X, y)
+
+joblib.dump(model, "prediction/disease_model.pkl")
+
+print("Model saved successfully")
