a6f83fbff1d4a624946972ee6910a275701d7cdc
Jainam249 <115345140+Jainam249@users.noreply.github.com>
2026-04-24 22:16:21 +0530

    Update wsgi.py

--- backend/core/wsgi.py
+++ backend/core/wsgi.py
@@ -9,8 +9,14 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 
 import os
 
+# Import Django's WSGI application factory
 from django.core.wsgi import get_wsgi_application
 
+# Set the default settings module for the 'core' project
+# This tells Django which settings to use when running the app
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
+# Create the WSGI application object
+# This is used by WSGI-compatible web servers (like Gunicorn or uWSGI)
+# to serve your Django application
 application = get_wsgi_application()
